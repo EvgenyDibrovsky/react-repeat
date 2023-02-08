f11af14818140fe9407649e1c2a37913785c7a56
Evgeny Dibrovsky <105384282+EvgenyDibrovsky@users.noreply.github.com>
2023-02-08 23:00:28 +0100

    com

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,31 +7,35 @@ export const App = () => {
       <Painting
         imageUrl={paintings[0].url}
         title={paintings[0].title}
-        author={paintings[0].author}
+        author={paintings[0].author.tag}
+        profileUrl={paintings[0].author.url}
         price={paintings[0].price}
-        quantity={paintings[0].uquantityrl}
+        quantity={paintings[0].quantity}
       />
       <Painting
         imageUrl={paintings[1].url}
         title={paintings[1].title}
-        author={paintings[1].author}
+        author={paintings[1].author.tag}
+        profileUrl={paintings[1].author.url}
         price={paintings[1].price}
-        quantity={paintings[1].uquantityrl}
+        quantity={paintings[1].quantity}
       />
       <Painting
         imageUrl={paintings[2].url}
         title={paintings[2].title}
-        author={paintings[2].author}
+        author={paintings[2].author.tag}
+        profileUrl={paintings[2].author.url}
         price={paintings[2].price}
-        quantity={paintings[2].uquantityrl}
+        quantity={paintings[2].quantity}
       />
 
       <Painting
         imageUrl={paintings[3].url}
         title={paintings[3].title}
-        author={paintings[3].author}
+        author={paintings[3].author.tag}
+        profileUrl={paintings[3].author.url}
         price={paintings[3].price}
-        quantity={paintings[3].uquantityrl}
+        quantity={paintings[3].quantity}
       />
     </div>
   );
--- src/components/Painting.jsx
+++ src/components/Painting.jsx
@@ -1,10 +1,10 @@
-const Painting = ({ imageUrl, title, author, price, quantity }) => {
+const Painting = ({ imageUrl, title, profileUrl, author, price, quantity }) => {
   return (
     <div>
       <img src={imageUrl} alt={title} width="480" />
       <h2>{title}</h2>
       <p>
-        Автор: <a href={author.url}>{author.tag}</a>
+        Автор: <a href={profileUrl}>{author}</a>
       </p>
       <p>Цена: {price} кредитов</p>
       <p>Доступность: {quantity} заканчиавется или есть в наличии</p>
