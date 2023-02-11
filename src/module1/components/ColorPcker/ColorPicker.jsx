b1498da1bf7b5a90a53675a1058bd7ab866cc69a
Evgeny Dibrovsky <105384282+EvgenyDibrovsky@users.noreply.github.com>
2023-02-11 09:47:49 +0100

    commit

--- src/module1/components/ColorPcker/ColorPicker.jsx
+++ src/module1/components/ColorPcker/ColorPicker.jsx
@@ -1,12 +1,12 @@
 // import css from './ColorPicker.css';
-const ColorPicker = options => {
+const ColorPicker = ({ options }) => {
   return (
     <div className="ColorPicker">
       <h2 className="ColorPicker__title">ColorPicker</h2>
       <div>
-        {options.map(option => 
-          <span className="ColorPicker__option">{option.label}</span>
-        )}
+        {options.map(option => {
+          <span className="ColorPicker__option">{option.label}</span>;
+        })}
       </div>
     </div>
   );
