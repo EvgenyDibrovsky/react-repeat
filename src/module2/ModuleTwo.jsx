6ae5b3aec052f5f204bd748c26e239e764eaac21
Evgeny Dibrovsky <105384282+EvgenyDibrovsky@users.noreply.github.com>
2023-02-13 12:46:36 +0100

    commit

--- src/module2/ModuleTwo.jsx
+++ src/module2/ModuleTwo.jsx
@@ -1,33 +1,32 @@
+import React, { Component } from 'react';
 import Section from '../components/Section/Section';
 import Container from '../components/Container/Container';
 import Counter from './components/Counter/Counter';
 import ColorPicker from './components/ColorPcker/ColorPicker';
-import TodoList from './components/TodoList/TodoList'
+import TodoList from './components/TodoList/TodoList';
+import initialTodo from '../data/todo.json';
+import initialColorPicker from '../data/colorpicker.json';
 
-const colorPickerOption = [
-  { label: 'red', color: '#ff0000' },
-  { label: 'green', color: '#00ff00' },
-  { label: 'blue', color: '#0000ff' },
-  { label: 'yellow', color: '#ffff00' },
-  { label: 'pink', color: '#ff00ff' },
-  { label: 'purple', color: '#800080' },
-  { label: 'orange', color: '#ffa500' },
-  { label: 'brown', color: '#a52a2a' },
-  { label: 'grey', color: '#808080' },
-];
+const colorPickerOption = initialColorPicker;
 
-const ModuleTwo = () => {
-  return (
-    <div>
-      <Section title="Модуль 2 События и состояние. Формы" type="white">
-        <Container>
-          <Counter initialValue={3} />
-          <ColorPicker options={colorPickerOption} />
-          <TodoList />
-        </Container>
-      </Section>
-    </div>
-  );
-};
+class ModuleTwo extends Component {
+  state = {
+    todos: initialTodo,
+  };
+  render() {
+    const { todos } = this.state;
+    return (
+      <div>
+        <Section title="Модуль 2 События и состояние. Формы" type="white">
+          <Container>
+            <Counter initialValue={3} />
+            <ColorPicker options={colorPickerOption} />
+            <TodoList todos={todos} />
+          </Container>
+        </Section>
+      </div>
+    );
+  }
+}
 
 export default ModuleTwo;
--- src/module2/components/TodoList/TodoList.jsx
+++ src/module2/components/TodoList/TodoList.jsx
@@ -1,7 +1,16 @@
-// import s from './TodoList.module.css'
+import s from './TodoList.module.css';
 
-const TodoList =({todos}) => {
-    return (<ul>TodoList</ul>)
-}
+const TodoList = ({ todos }) => {
+  return (
+    <ul className={s.todolist}>
+      {todos.map(({ id, text }) => (
+        <li className={s.listItem} key={id}>
+          <p className={s.textList}>{text}</p>
+          <button className={s.todoDelBtn}> - </button>
+        </li>
+      ))}
+    </ul>
+  );
+};
 
-export default TodoList
+export default TodoList;
