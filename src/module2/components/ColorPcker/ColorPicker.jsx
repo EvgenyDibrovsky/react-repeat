24bdebd3502ccf4db9bca8870114249c40c5595d
Evgeny Dibrovsky <105384282+EvgenyDibrovsky@users.noreply.github.com>
2023-02-13 10:17:04 +0100

    commit

--- src/module2/components/ColorPcker/ColorPicker.jsx
+++ src/module2/components/ColorPcker/ColorPicker.jsx
@@ -2,18 +2,36 @@ import s from './ColorPicker.module.css';
 import React, { Component } from 'react';
 
 class ColorPicker extends Component {
+  state = {
+    activeOptionIndex: 1,
+  };
+
+  makeOptionClassName = index => {
+    const optionClases = [s.option];
+    if (index === this.state.activeOptionIndex) {
+      optionClases.push(s.activeOption);
+    }
+    return optionClases.join(' ');
+  };
+
   render() {
     return (
       <div className={s.container}>
         <h2 className={s.title}>ColorPicker</h2>
         <div>
-          {this.props.options.map(({ label, color }) => (
-            <button
-              key={label}
-              className={s.option}
-              style={{ backgroundColor: color }}
-            ></button>
-          ))}
+          {this.props.options.map(({ label, color }, index) => {
+            const optionClassName = this.makeOptionClassName(index);
+
+            return (
+              <button
+                key={label}
+                className={optionClassName}
+                style={{
+                  backgroundColor: color,
+                }}
+              ></button>
+            );
+          })}
         </div>
       </div>
     );
